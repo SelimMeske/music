1856d641881bac6ddaaceea859599bc68be7dcb5
SelimMeske <mesic.selim@outlook.com>
2021-03-01 16:47:14 +0100

    added login function

--- music/userauth/views.py
+++ music/userauth/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_view(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
     return render(request, 'user_auth/custom_signup.html')
+
+def login_view(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return redirect('login')
+
+    return render(request, 'user_auth/custom_login.html')
