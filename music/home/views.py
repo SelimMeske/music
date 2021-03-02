57a070a83261a76e6b99b52976fca30426752002
SelimMeske <mesic.selim@outlook.com>
2021-03-02 23:04:18 +0100

    added play music func

--- music/home/views.py
+++ music/home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from song.models import Song
 
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'home/home.html')
+
+    songs = Song.objects.all()
+
+    return render(request, 'home/home.html', {'songs': songs})
