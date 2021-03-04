bd56d60f6793aee19abdb2f3ff064592905e96a9
SelimMeske <mesic.selim@outlook.com>
2021-03-04 22:20:30 +0100

    Fixed context processor bug

--- music/music/context_processors.py
+++ music/music/context_processors.py
@@ -5,8 +5,7 @@ def user_info(request):
     if request.user.is_authenticated:
         user = request.user
         user_info = UserProfile.objects.all().filter(user_fk=user)
+        print(user_info[0].is_artist)
+        return {'userinfo': user_info[0]}
 
-        return {'userinfo': user_info}
-    else:
-        return None
-
+    return {'userinfo': ''}
