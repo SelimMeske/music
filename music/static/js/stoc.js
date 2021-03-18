6b1a7147b151e547a28bfd00bb04631570bffc3d
SelimMeske <mesic.selim@outlook.com>
2021-03-18 23:51:25 +0100

    Removed channels

--- music/static/js/stoc.js
+++ music/static/js/stoc.js
@@ -1,30 +1,4 @@
-let notifySocket;
-let ws_scheme = window.location.protocol == 'https' ? 'wss' : 'ws';
-
 let notify_container = document.querySelector('.custom-messages-container');
-
-function connectSocket() {
-
-    notifySocket = new WebSocket(
-        ws_scheme + '://' + window.location.host + '/notify'
-    );
-
-    notifySocket.onmessage = function(e) {
-        console.log('Here is the message')
-        console.log(e.data);
-    };
-
-    notifySocket.onclose = function(e) {
-        console.error("error");
-        setTimeout(connectSocket, 1000);
-    };
-
-    notifySocket.onopen = function(e) {
-        console.log("Socket connected");
-    }
-};
-
-connectSocket();
 
 function notifyControl(message){
     let notify_div = document.createElement("div");
