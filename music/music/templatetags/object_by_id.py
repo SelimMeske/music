79b6951257e14610349f37ee810a7190e12274f7
SelimMeske <mesic.selim@outlook.com>
2021-03-19 19:22:39 +0100

    Improved ui

--- music/music/templatetags/object_by_id.py
+++ music/music/templatetags/object_by_id.py
@@ -7,15 +7,11 @@ register = template.Library()
 @register.filter(name='object_by_id')
 def get_by_id(value, arg):
 
-
     if arg.is_authenticated:
-        print('is')
         try:
             fav_song = FavoriteSong.objects.get(user_fk=arg, song_fk=value)
-            print('is favor')
             return 'fav-btn favourite-song'
         except ObjectDoesNotExist:
-            print('is not favor')
             return 'fav-btn'
     else:
         return 'fav-btn'
--- music/static/js/play-func.js
+++ music/static/js/play-func.js
@@ -4,8 +4,6 @@ let all_buttons = document.querySelectorAll('.audio-play');
 for (i = 0; i < all_buttons.length; i++) {
     all_buttons[i].addEventListener('click', function(event){
         current_song = event.target;
-        console.log(current_song)
-        console.log(current_song.parentElement)
 
         let current_playing = document.querySelector('.playing');
 
