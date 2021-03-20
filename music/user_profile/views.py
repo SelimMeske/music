b2687c3a2a3b81b73a1a21e8cdb812b7c1317b7e
SelimMeske <mesic.selim@outlook.com>
2021-03-20 14:46:07 +0100

    added waveform visualization on my profile page

--- music/user_profile/views.py
+++ music/user_profile/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from song.models import Song
+from django.core.serializers import serialize
+import json
 
 # Create your views here.
 def profile_view(request):
-    return render(request, 'profile/my_profile.html')
+
+    user = request.user
+
+    if user.is_authenticated:
+        songs = Song.objects.all().filter(artist=user)
+        serialized_songs = serialize('json', songs, ensure_ascii=False)
+    else:
+        songs = ''
+
+    return render(request, 'profile/my_profile.html', {'songs': songs, 'ser_songs': json.dumps(serialized_songs)})
