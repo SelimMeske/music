b743df20d8cda97f8adc04decb44721dd9a17d7c
SelimMeske <mesic.selim@outlook.com>
2021-03-21 22:49:46 +0100

    fixed logger

--- music/music/context_processors.py
+++ music/music/context_processors.py
@@ -9,7 +9,9 @@ def user_info(request):
     if request.user.is_authenticated:
         user = request.user
         is_user_there = UserProfile.objects.all().filter(user_fk=user)
-        logger.error('This is the user ' + user)
+        logger.info('This is the user ')
+        logger.error(user)
+        logger.info('This is the user info')
         logger.error(is_user_there)
 
         if is_user_there:
