82aacd93c2a178634e5e06f8e7f8860775bf99f8
SelimMeske <mesic.selim@outlook.com>
2021-03-22 20:45:57 +0100

    fixed bug in template

---
+++ music/music/templatetags/calculations.py
@@ -0,0 +1,9 @@
+from django import template
+from django.core.exceptions import ObjectDoesNotExist
+
+register = template.Library()
+
+@register.filter(name='calculations')
+def calculations(value, args):
+    if args == 'modulus':
+        return value%6
