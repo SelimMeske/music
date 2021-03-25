c29ae2b3f996f193079b865be35b2c1980e65396
SelimMeske <mesic.selim@outlook.com>
2021-03-25 01:29:54 +0100

    added textfield

--- music/song/models.py
+++ music/song/models.py
@@ -7,6 +7,7 @@ class Song(models.Model):
     name = models.CharField(max_length=420)
     image = models.ImageField(upload_to='images/')
     audio = models.FileField(upload_to='audio/')
+    audio_data = models.TextField()
     artist = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
