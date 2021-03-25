6f70dc12dc663419ab57c2e7e885dec020bf9559
SelimMeske <mesic.selim@outlook.com>
2021-03-25 10:17:21 +0100

    added delete functionality

--- music/create_song/views.py
+++ music/create_song/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from music.forms import CustomUploadForm
 from user_profile.models import UserProfile
+from song.models import Song
+import json
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -28,3 +32,16 @@ def create_song(request):
     form = CustomUploadForm()
 
     return render(request, 'create_song/create_song.html', {'form': form})
+
+def delete_song(request):
+
+    user = request.user
+    if user.is_authenticated:
+        song_id = json.loads(request.body)['song-id']
+        try:
+            Song.objects.get(id=song_id).delete()
+        except ObjectDoesNotExist:
+            return JsonResponse({'message':'There is an error occured.'})
+
+        return JsonResponse({'message': 'Song deleted.'})
+
