823cb661d6aac77a9625a44d8572aa3ad01822e6
SelimMeske <mesic.selim@outlook.com>
2021-05-31 17:59:55 +0200

    added requirements.txt and set cors policy

--- music/settings/base.py
+++ music/settings/base.py
@@ -33,16 +33,23 @@ INSTALLED_APPS = [
     'ckeditor',
     'blog',
     'test',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+        "http://localhost:4200",
 ]
 
 REST_FRAMEWORK = {
