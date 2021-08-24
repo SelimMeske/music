8175125fffb1b3b41009001dbf9eec07443e3f2b
SelimMeske <mesic.selim@outlook.com>
2021-08-24 09:36:56 +0200

    Added new endpoint

--- music/music/main_api/urls.py
+++ music/music/main_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import SongView
+from .views import SingleSongView, SongView
 
 urlpatterns = [
-    path('songs', SongView.as_view())
+    path('songs', SongView.as_view()),
+    path('song-by-id', SingleSongView.as_view())
 ]
--- music/music/main_api/views.py
+++ music/music/main_api/views.py
@@ -1,3 +1,4 @@
+from django.core.serializers import serialize
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,3 +14,10 @@ class SongView(APIView):
     
     def post(slef, request):
         return Response(0)
+
+class SingleSongView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        single_song = Song.objects.get(request.id)
+        serializer = SongSerializer(single_song, many=True)
+        return Response(serializer.data)
